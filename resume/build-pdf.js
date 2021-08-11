15db9cb83074c968942a4bfbac1a161d457bccd1
Brandon Summers <brandon@brandonsummers.name>
2021-08-11 06:02:03 -0700

    Adding resume.

---
+++ resume/build-pdf.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-console */
+const path = require('path');
+const playwright = require('playwright');
+
+async function main() {
+  const htmlFile = path.resolve('./resume.html');
+
+  const browser = await playwright.chromium.launch({
+    args: ['--disable-dev-shm-usage'],
+  });
+  const page = await browser.newPage();
+  await page.emulateMedia({ media: 'print' });
+  await page.goto(`file://${htmlFile}`, { waitUntil: 'networkidle0' });
+
+  await page.pdf({
+    format: 'Letter',
+    margin: {
+      bottom: '0.4in',
+      left: '0.4in',
+      right: '0.4in',
+      top: '0.4in',
+    },
+    path: 'resume.pdf',
+    preferCSSPageSize: true,
+    printBackground: true,
+  });
+
+  await browser.close();
+}
+
+main().catch(err => {
+  console.error(err);
+  process.exit(1);
+});
